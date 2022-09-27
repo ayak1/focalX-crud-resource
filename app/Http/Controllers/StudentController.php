54e025ead09f5ced7903ddb56d1282a592e9f285
Hudaghazal <hudaygh2000@gmail.com>
2022-09-26 15:43:49 -1200

    update and create function

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+        $students = Student::all();
+        return view('student.index' , compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('student.create');
+    }
+     
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $student=Student::create([ 
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'age' => $request->age,
+            'address' => $request->address,
+
+        ]);
+        
+        return redirect()->route('student.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+    {
+        return view('student.student', compact('student'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Student $student)
+    {
+      return view('student.edit' , compact('student'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Student $student)
+    {
+        
+        $request->validate([
+            'name' => 'required',
+            'phone' => 'required',
+            'age' => 'required',
+            'address' => 'required',
+        ]);
+    
+        $student->update($request->all());
+
+        return  redirect()->route('student.index');
+    
+    }
+     
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        $student->delete();
+        return redirect()->route('student.index');
+
+    }
+}
